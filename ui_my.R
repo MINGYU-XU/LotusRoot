4d790cea909fcfd3a6a804564a11f5b59040d808
XU Mingyu <s2045156@ed.ac.uk>
2021-06-08 23:37:54 +0100

    ui_06082337

--- ui_my.R
+++ ui_my.R
@@ -9,9 +9,9 @@ library(shiny)
 library(tidyverse)
 
 # ui
-# dashboard_header
+# dashboard_header ----------------------------------
 header <- dashboardHeader(
-  title="MY LIMS",
+  title="My LIMS",
   dropdownMenu(
     type="messages",
     #add Items
@@ -26,30 +26,70 @@ header <- dashboardHeader(
   )
 )
 
-# dashboard_sidebar 
+# dashboard_sidebar -----------------------------------
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Home", tabName = "home", icon = icon(name="home")),
-    menuItem("My Project", icon = icon(name="dna"), tabName = "my_project",
-             badgeLabel = "new", badgeColor = "green"),
-    menuItem("About Us", tabName = "about_us", icon = icon(name="user-friends")),
+    menuItem("My Project", icon = icon(name="dna"), tabName = "myproject",
+             #badgeLabel = "new", badgeColor = "green"
+             menuItem("Creat New Project", tabName = "creat_new_project", icon = icon(name = "plus-circle")),
+             menuItem("My Current Projects", tabName = "current_project", icon = icon(name = "th-list"))
+             ),
+    menuItem("About Us", tabName = "aboutus", icon = icon(name="user-friends")),
     menuItem("FAQ", tabName = "FAQ", icon = icon(name="question-circle"))
   )
 )
 
-# dashboard_body
+# dashboard_body ------------------------------------------
 body <- dashboardBody(
   tabItems(
-    tabItem(tabName = "Home",
-            h2("Home")
+    tabItem(tabName = "home",
+            h2("Home"),
+            ## user_name
+            textInput(inputId = "user_name", 
+                      label = "User Name :"),
+            ## password
+            passwordInput(inputId = "password", 
+                          label = "Password :"),
+            ## email
+            textInput(inputId = "email",
+                      label = "Email :"),
+            ## log in
+            submitButton(text = " Log in / Register",
+                         icon = icon(name = "sign-in-alt"),
+                         width = "200px")
+            ),
+    
+    tabItem(tabName = "creat_new_project",
+            h2("Creat New Project"),
+            ## project_name
+            textInput(inputId = "project_name", 
+                      label = "Project Name :"),
+            ## project_password
+            passwordInput(inputId = "project_password", 
+                          label = "Project Password :"),
+            ## administrator
+            textInput(inputId = "administrator",
+                      label = "Administrator :"),
+            fileInput(inputId = "fileinput",
+                      label = "Upload files:"),
+            ## log in
+            submitButton(text = " Submit ",
+                         icon = icon(name = "sign-in-alt"),
+                         width = "100px")
     ),
     
-    tabItem(tabName = "My Project",
-            h2("My Project")
-    ),
+    tabItem(tabName = "current_project",
+            h2("My Current Projects"),
+            
+            # list of current projects
+            
+            
+            
+            ),
     
-    tabItem(tabName = "About us",
-            h2("About us")
+    tabItem(tabName = "aboutus",
+            h2("About Us")
     ),
       
     tabItem(tabName = "FAQ",
@@ -59,14 +99,15 @@ body <- dashboardBody(
 )
 
 
+
 #ui
 ui <- dashboardPage(
-  header,
-  sidebar,
-  body
+        header,
+        sidebar,
+        body
 )
 
-
+---------------------------------------------------------
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
